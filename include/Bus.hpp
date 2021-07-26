6d5e5be5a3f99fd423e9d9af724e539a7958fe5f
OnkarKunjir <onkarkunjir8@gmail.com>
2021-07-26 11:14:19 +0530

    Initial commit.
    Created project structure and added class to emulate bus.

---
+++ include/Bus.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+
+/**
+ * Class emulates behaviour of the bus.
+ *
+ * Bus can be used to transfer 1 byte data from 2 byte addressable memory.
+ */
+class Bus {};
